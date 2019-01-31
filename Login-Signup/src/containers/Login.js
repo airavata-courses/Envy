927a434b757117b5b2bb8cb5ce26991eec95e882
Alakh <araghuv@iu.edu>
2019-01-31 06:59:56 -0500

    Issue #5 completed

---
+++ Login-Signup/src/containers/Login.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      error: "true"
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    const url = "http://localhost:3000/login";
+    let data = { username: this.state.email, password: this.state.password };
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ error: data.status.error });
+        if (data.status.error === "false") {
+          console.log("user logged in");
+          this.props.history.push("/");
+        }
+      })
+      .catch(error => console.error("Error:", error));
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="Login container">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            className="btn btn-outline-primary"
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
