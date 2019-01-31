8d59abdb3320e205b5e56984cb2d72f6ac0090a3
Alakh <araghuv@iu.edu>
2019-01-31 16:29:47 -0500

    Issue #2 completed

---
+++ Login-Signup/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Travel Guide</h1>
+          <p>One stop for all your travel needs</p>
+        </div>
+      </div>
+    );
+  }
+}
