7818ccfed607bbaef7c1568a9b8dbd254b80c617
Alakh <araghuv@iu.edu>
2019-01-31 07:14:36 -0500

    #2 completed

--- travel/index.js
+++ travel/index.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const db = require("./queries");
+const port = 3000;
+var cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended: true
   })
-)
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+);
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
-app.get('/login', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/login', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.post('/users/:id', db.deleteUser)
+app.get("/login", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/login", db.authorizeUser);
+app.post("/signup", db.addUser);
+app.put("/users/:id", db.updateUser);
+app.post("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
--- travel/queries.js
+++ travel/queries.js
@@ -1,94 +1,133 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'password',
-  port: 5432,
-})
+  user: "me",
+  host: "localhost",
+  database: "api",
+  password: "password",
+  port: 5432
+});
 
 var verifyUser = (username, password, response) => {
-  var success = 0
-  pool.query('SELECT * FROM user_authentication WHERE name=$1 and password=$2', [username, password], (error, results) => {
-    if (error) {
-      throw error
+  var success = 0;
+  pool.query(
+    "SELECT * FROM user_travel WHERE email=$1 and password=$2",
+    [username, password],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      success = results.rowCount;
+      if (success) {
+        response.status(200).json({
+          status: {
+            type: "success",
+            message: "Authenticated",
+            code: "200",
+            error: "false"
+          }
+        });
+      }
     }
-    success = results.rowCount
-    if(success)
-    {
-      response.status(200).json({ 
-      status: { type: "success", message: "Authenticated", code: "200", error: "false" },
-      })
-    }
-  })
-}
+  );
+};
 
 const getUsers = (request, response) => {
-  pool.query('SELECT * FROM user_authentication ORDER BY id ASC', (error, results) => {
+  pool.query("SELECT * FROM user_travel ORDER BY id ASC", (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
-    response.status(200).json(results.rows)
-  })
-}
+    response.status(200).json(results.rows);
+  });
+};
 
 const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('SELECT * FROM user_authentication WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-
-const createUser = (request, response) => {
-  const { username, password } = request.body
-  console.log(request.body.username);
-  // pool.query('INSERT INTO user_authentication (name, password) VALUES ($1, $2)', [username, password], (error, results) => {
-  //   if (error) {
-  //     throw error
-  //   }
-  //   response.status(201).send(request.body)
-  // })
-  verifyUser(username, password, response)
-}
-
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, password } = request.body
+  const id = parseInt(request.params.id);
 
   pool.query(
-    'UPDATE user_authentication SET name = $1, password = $2 WHERE id = $3',
+    "SELECT * FROM user_travel WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+};
+
+const authorizeUser = (request, response) => {
+  const { username, password } = request.body;
+  //console.log(request.body.username);
+  verifyUser(username, password, response);
+};
+
+const addUser = (request, response) => {
+  const { username, password } = request.body;
+  pool.query(
+    "INSERT INTO user_travel (email, password, dob) VALUES ($1, $2, '01-01-1996')",
+    [username, password],
+    (error, results) => {
+      if (error) {
+        response.status(400).json({
+          status: {
+            type: "failure",
+            message: error,
+            code: "400",
+            error: "true"
+          }
+        });
+      } else {
+        console.log(results);
+        response.status(200).json({
+          status: {
+            type: "success",
+            message: "User added to the database",
+            code: "200",
+            error: "false"
+          }
+        });
+      }
+    }
+  );
+};
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, password } = request.body;
+
+  pool.query(
+    "UPDATE user_travel SET name = $1, password = $2 WHERE id = $3",
     [name, password, id],
     (error, results) => {
       if (error) {
-        throw error
+        throw error;
       }
-      response.status(200).send(`User modified with ID: ${id}`)
+      console.log(results);
+      response.status(200).send(`User modified with ID: ${id}`);
     }
-  )
-}
+  );
+};
 
 const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
+  const id = parseInt(request.params.id);
 
-  pool.query('DELETE FROM user_authentication WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
+  pool.query(
+    "DELETE FROM user_travel WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User deleted with ID: ${id}`);
     }
-    response.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
-
-
+  );
+};
 
 module.exports = {
   getUsers,
   getUserById,
-  createUser,
+  authorizeUser,
+  addUser,
   updateUser,
-  deleteUser,
-}
+  deleteUser
+};
