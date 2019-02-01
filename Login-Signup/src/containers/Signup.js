197e59ced389fdf5abb021f8a1827e8e0f715f27
Alakh <araghuv@iu.edu>
2019-02-01 15:32:18 -0500

    Added relavent files for UI controler

---
+++ Login-Signup/src/containers/Signup.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import Image from "./image";
+import "./Signup.css";
+
+export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      confirmationCode: "",
+      newUser: null
+    };
+  }
+
+  validateForm() {
+    return (
+      this.state.email.length > 0 &&
+      this.state.password.length > 0 &&
+      this.state.password === this.state.confirmPassword
+    );
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ isLoading: true });
+    const url = "http://localhost:3000/signup";
+    let data = { username: this.state.email, password: this.state.password };
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ error: data.status.error });
+        if (data.status.error === "false") {
+          console.log("user has signed in");
+          this.props.history.push("/");
+        }
+      })
+      .catch(error => console.error("Error:", error));
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="classGrid">
+        <Image />
+        <div className="Signup cardsignup">
+          <form onSubmit={this.handleSubmit}>
+            <FormGroup controlId="email" bsSize="large">
+              <ControlLabel>Email</ControlLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+              <ControlLabel>Password</ControlLabel>
+              <FormControl
+                value={this.state.password}
+                onChange={this.handleChange}
+                type="password"
+              />
+            </FormGroup>
+            <FormGroup controlId="confirmPassword" bsSize="large">
+              <ControlLabel>Confirm Password</ControlLabel>
+              <FormControl
+                value={this.state.confirmPassword}
+                onChange={this.handleChange}
+                type="password"
+              />
+            </FormGroup>
+            <Button
+              className="w3-btn w3-blue"
+              block
+              bsSize="large"
+              disabled={!this.validateForm()}
+              type="submit"
+            >
+              Signup
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
