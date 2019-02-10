11171d6992648fa6caf70b8e0aad52545d1454fe
alakhsingh <alakhsingh2009@gmail.com>
2019-02-09 20:21:34 -0500

    #45 Bug 1/2 fixed

--- initializer.sql
+++ initializer.sql
@@ -1,14 +1,18 @@
 CREATE ROLE me WITH LOGIN PASSWORD 'password';
+DO
+$do$
+BEGIN
+	IF NOT EXISTS(
+			SELECT FROM pg_catalog.pg_roles where rolname = 'me')
+			THEN
+			CREATE ROLE me WITH LOGIN PASSWORD 'password';
+		END IF;
+	END
+$do$;
+
 ALTER ROLE me CREATEDB;
 
+
+DROP DATABASE IF EXISTS api;
 CREATE DATABASE api;
 \c api
-
-create table user_travel(email text primary key, password text, dob date);
-create table airport_index(airport text, latitude double precision, longitude double precision, iata text, primary key(latitude, longitude));
-create table search_details(id serial primary key, car_carrier text, flight_carrier text, total_price double precision,car_source text, car_destination text, flight_source text, flight_destination text, date text, search_id text, car_source_price double precision, car_destination_price double precision, flight_price double precision);
-
-Grant all on airport_index to me;
-Grant all on search_details to me;
-Grant all on user_travel to me;                                                                                                                       
-GRANT USAGE, SELECT ON SEQUENCE search_details_id_seq TO me;
