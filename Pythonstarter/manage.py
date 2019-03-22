227662e15da2b140031ebd0f7bcdf191a10a0898
thumblas <oceanichorizon95@gmail.com>
2019-03-22 17:22:50 -0400

    Removed Python consul autoregistry changes

--- Pythonstarter/manage.py
+++ Pythonstarter/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-import consul
+#import consul
 import requests
 
 if __name__ == "__main__":
@@ -9,9 +9,9 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
-    c = consul.Consul()
-    f = requests.request('GET', 'http://myip.dnsomatic.com')
-    res = c.agent.service.register('pythonservice',address=f.content,port=8000)
+   # c = consul.Consul()
+   # f = requests.request('GET', 'http://myip.dnsomatic.com')
+   # res = c.agent.service.register('pythonservice',address=f.content,port=8000)
     ''',check={
                 "DeregisterCriticalServiceAfter": "3m",
                 "http": "http://{}:{}/v1/health".format(f.content, 8500),
