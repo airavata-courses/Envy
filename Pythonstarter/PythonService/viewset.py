6f27969135ddf55a360dc154945b8c53a9b44f02
varun-mach <vmaching@iu.edu>
2019-04-02 18:37:50 -0400

    node branch

--- Pythonstarter/PythonService/viewset.py
+++
@@ -1,8 +0,0 @@
-from .models import *
-from .serializers import *
-
-from rest_framework import viewsets
-
-class UserTravelViewSet(viewsets.ModelViewSet):
-	queryset = UserTravel.objects.all()
-	serializer_class = UserTravelSerializer
