e3dba8b33f5133b8166413277bf50b2ca68b0edc
alakhsingh <alakhsingh2009@gmail.com>
2019-04-14 18:15:31 -0400

    support UI changes

--- Pythonstarter/PythonService/views.py
+++ Pythonstarter/PythonService/views.py
@@ -113,7 +113,7 @@ def Itenary(request):
 	        print(temp)
 	        #search_id = '2'
 	        queryset = SearchDetails.objects.filter(search_id=str(temp)).all()
-	        queryset2 = SearchDetails.objects.raw("select * from search_details where search_id= %s and total_price <= all(select total_price from search_details where search_id= %s )",[str(temp),str(temp)])
+	        queryset2 = SearchDetails.objects.raw("select * from search_details where search_id= %s)",[str(temp)])
 	        #serializer_class = UserTravelSerializer
 	        # ret = []
 	        # for o in result:
