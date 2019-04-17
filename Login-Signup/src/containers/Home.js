908d0d3a855e8bce83452b2c747dc44b2e0cb11b
Sbf Firodiya <shfirodi@iu.edu>
2019-04-17 19:27:14 -0400

    Update Home.js

--- Login-Signup/src/containers/Home.js
+++ Login-Signup/src/containers/Home.js
@@ -45,7 +45,7 @@ export default class Home extends Component {
         date: this.state.date,
         search_id: this.state.search_id
       };
-      const url = "http://149.165.170.230:30025/getAirport?origin="+this.state.origin+"&destination="+this.state.destination+"&date="+this.state.date+"&search_id="+this.state.search_id;
+      const url = "/javabackend/getAirport?origin="+this.state.origin+"&destination="+this.state.destination+"&date="+this.state.date+"&search_id="+this.state.search_id;
       console.log("url", url);
       fetch(url, {
         method: "GET",
