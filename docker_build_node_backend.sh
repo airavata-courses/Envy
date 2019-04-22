379a49527ab4c5c7eb047cabdc01353494a51709
Sbf Firodiya <shfirodi@iu.edu>
2019-04-22 18:30:22 -0400

    Add files via upload

---
+++ docker_build_node_backend.sh
@@ -0,0 +1,12 @@
+cd travel
+#mvn install
+#sudo docker login --username = varunmachingal92 --password = sga@2019
+sudo docker build -t varunmachingal92/node-server .
+sudo docker login -u varunmachingal92 -p sga@2019
+sudo docker push varunmachingal92/node-server
+cd ..
+pwd
+cd airports
+sudo docker build -t varunmachingal92/airports-node .
+sudo docker push varunmachingal92/airports-node
+
