b08c8213a40b630ac66f9a9694ee0e2c600563b0
Sbf Firodiya <shfirodi@iu.edu>
2019-04-22 18:28:38 -0400

    Add files via upload

---
+++ docker_build_ui.sh
@@ -0,0 +1,10 @@
+cd Login-Signup
+#mvn install
+#sudo docker login --username = varunmachingal92 --password = sga@2019
+sudo docker build -t varunmachingal92/ui-node .
+sudo docker login -u varunmachingal92 -p sga@2019
+sudo docker push varunmachingal92/ui-node
+
+
+
+
