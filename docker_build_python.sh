c9b851e239be82c6297e626c9a50653852707966
Sbf Firodiya <shfirodi@iu.edu>
2019-04-22 18:31:25 -0400

    Add files via upload

---
+++ docker_build_python.sh
@@ -0,0 +1,8 @@
+#cd Envy
+cd Pythonstarter
+#mvn install
+#sudo docker login --username = varunmachingal92 --password = sga@2019
+sudo docker build -t varunmachingal92/python-node .
+sudo docker login -u varunmachingal92 -p sga@2019
+sudo docker push varunmachingal92/python-node
+
