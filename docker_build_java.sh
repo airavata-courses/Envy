2fc3c9e362ecc4c85f13d1b1b7ba331b49d8f083
Sbf Firodiya <shfirodi@iu.edu>
2019-04-22 18:30:53 -0400

    Add files via upload

---
+++ docker_build_java.sh
@@ -0,0 +1,7 @@
+#cd Envy
+cd starter_envy
+mvn install
+#sudo docker login --username = varunmachingal92 --password = sga@2019
+sudo docker build -t varunmachingal92/java-node .
+sudo docker login -u varunmachingal92 -p sga@2019
+sudo docker push varunmachingal92/java-node
