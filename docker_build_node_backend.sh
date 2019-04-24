8991335c9c0a9b460ef81c0405b354a04dcfb975
Sbf Firodiya <shfirodi@iu.edu>
2019-04-24 18:12:50 -0400

    Update docker_build_node_backend.sh

--- docker_build_node_backend.sh
+++ docker_build_node_backend.sh
@@ -1,12 +1,12 @@
 cd travel
 #mvn install
 #sudo docker login --username = varunmachingal92 --password = sga@2019
-sudo docker build -t varunmachingal92/node-server .
+sudo docker build -t varunmachingal92/node-server-blue .
 sudo docker login -u varunmachingal92 -p sga@2019
-sudo docker push varunmachingal92/node-server
+sudo docker push varunmachingal92/node-server-blue
 cd ..
 pwd
 cd airports
-sudo docker build -t varunmachingal92/airports-node .
-sudo docker push varunmachingal92/airports-node
+sudo docker build -t varunmachingal92/airports-node-blue .
+sudo docker push varunmachingal92/airports-node-blue
 
