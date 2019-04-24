4c5bead875e285ba3ce2c4e8bb2bc6bfded706b5
Sbf Firodiya <shfirodi@iu.edu>
2019-04-24 17:55:24 -0400

    Update docker_build_ui.sh

--- docker_build_ui.sh
+++ docker_build_ui.sh
@@ -1,8 +1,8 @@
 cd ui-updated/
 #sudo docker login --username = varunmachingal92 --password = sga@2019
-sudo docker build -t varunmachingal92/ui-node .
+sudo docker build -t varunmachingal92/ui-node-blue .
 sudo docker login -u varunmachingal92 -p sga@2019
-sudo docker push varunmachingal92/ui-node
+sudo docker push varunmachingal92/ui-node-blue
 
 
 
