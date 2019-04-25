8b56bcede961a3d540bb1b8361d96f84d751617c
Varun Vinod Machingal <varun.machingal@gmail.com>
2019-04-25 18:37:51 -0400

    Image name changed

--- docker_build_java.sh
+++ docker_build_java.sh
@@ -2,6 +2,6 @@
 cd starter_envy
 mvn install
 #sudo docker login --username = varunmachingal92 --password = sga@2019
-sudo docker build -t varunmachingal92/java-node .
+sudo docker build -t varunmachingal92/java-node-green .
 sudo docker login -u varunmachingal92 -p sga@2019
-sudo docker push varunmachingal92/java-node
+sudo docker push varunmachingal92/java-node-green
