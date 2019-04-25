1de2e7464124193d0e2db82ca5a6e0f416f1a169
alakhsingh <alakhsingh2009@gmail.com>
2019-04-25 12:34:40 -0400

    cors issue

--- travel/index.js
+++ travel/index.js
@@ -16,18 +16,7 @@ app.use(
   })
 );
 
-app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  next();
-});
-
+app.options('*', cors());
 
 app.get("/", (request, response) => {
   response.json({ info: "Authentication service is up and running" });
